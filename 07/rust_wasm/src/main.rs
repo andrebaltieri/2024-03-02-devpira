796003e0813070e4ae697d10611d333b94629284
Andre Baltieri <andrebaltieri@hotmail.com>
2024-03-03 19:18:39 -0300

    Subindo as demos

---
+++ 07/rust_wasm/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let num1: u32 = 10;
+    let num2: u32 = 20;
+
+    let result1 = num1 * num2;
+    let result2 = num1 * num2 * 2;
+    let result3 = result1 + result2;
+
+    println!("Resultado: {}", result3);
+}
